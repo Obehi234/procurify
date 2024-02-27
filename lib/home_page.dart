58388af063208d28d8cfb864a19d55c113212bd0
obehi234 <obehi.ataga234@gmail.com>
2024-02-27 10:14:06 +0100

    Appbar completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-     appBar: AppBar(
-      leading: Image.asset('assets/images/procurify_logo.svg'),
-      actions: [
-        OutlinedButton(onPressed: () {}, child: Text('Get Started'))
-
-      ],
-
-     ),
-
+    return Scaffold(
+      appBar: AppBar(
+        leading: SvgPicture.asset('assets/images/logoP.svg'),
+        actions: [
+          OutlinedButton(onPressed: () {}, child: const Text('Get Started')),
+          SizedBox(
+            width: 8,
+          ),
+          const Icon(Icons.menu)
+        ],
+      ),
     );
   }
 }
